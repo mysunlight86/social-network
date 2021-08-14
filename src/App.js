1af857b4a89d052dce0538562a856ff4245f84fd
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2021-08-14 23:24:39 +0300

    feat: Создать компонент Message, отображающий переданный ему пропсом текст

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Message from './Message';
 
 function App() {
   return (
     <div className="App">
-      
+      <Message text="Переданный текст" />
     </div>
   );
 }
