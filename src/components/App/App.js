3ef37db582632c788d29f26b8166f30d86b02258
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2021-08-16 17:49:13 +0300

    feat: добавить код с урока и стилизовать

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,39 @@
+import React, { useState, useEffect, useCallback } from "react";
 import './App.css';
 import Message from '../Message/Message';
 
 function App() {
+  const [inputMessage, setInputMessage] = useState("");
+  const [messagesArray, setMessagesArray] = useState([]);
+
+  const onSendMessage = () => {
+    setMessagesArray((prev) => [...prev, inputMessage]);
+    setInputMessage("");
+  };
+
   return (
     <div className="app">
-      <Message text="Переданный текст" />
+      <div className="messageList">
+        {messagesArray.map((message, i) => (
+          <div key={i}>{message}</div>
+        ))}
+      </div>
+
+      <div className="inputWrapper">
+        <input
+          className="input"
+          value={inputMessage}
+          onChange={(e) => setInputMessage(e.target.value)}
+          onKeyDown={({key}) => {
+            if (key === 'Enter') {
+              console.log('Enter')
+              onSendMessage();
+            }
+          }}
+        />
+        <button onClick={onSendMessage}>Отправить</button>
+      </div>
+      {/* <Message text="Переданный текст" /> */}
     </div>
   );
 }
