1f1df0aba67682460941d6dbbffe1ddd5a464d6b
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2021-08-23 18:26:33 +0300

    feat: добавить ссылки на главной странице

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,32 @@
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+    homeContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    homeList: {
+        margin: 0,
+        padding: 0,
+        listStyleType: 'none',
+    },
+    homeLink: {
+        textDecoration: 'none',
+    }
+}));
+
 const Home = () => {
+    const classes = useStyles();
+
     return (
-        <div>Домашняя страница</div>
+        <section className={classes.homeContainer}>
+            <h1>Домашняя страница</h1>
+            <ul className={classes.homeList}>
+                <li><Link to="/chat" className={classes.homeLink}>Чат</Link></li>
+                <li><Link to="/profile" className={classes.homeLink}>Профиль</Link></li>
+            </ul>
+        </section>
     );
 };
 
