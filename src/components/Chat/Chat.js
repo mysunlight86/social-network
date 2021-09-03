470ca99a8368c718963c3ccc255c51fa166d9ef9
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2021-09-03 19:49:29 +0300

    feat: добавить в чат переключатель с маршрутом для переключения чатов по id

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Switch, Route } from "react-router-dom";
 import InputMessage from './InputMessage';
 import MessageList from './MessageList';
 import MenuList from '@material-ui/core/MenuList';
@@ -6,94 +7,98 @@ import MenuItem from '@material-ui/core/MenuItem';
 import { makeStyles } from '@material-ui/core/styles';
 
 const chats = [
-    {
-        id: 0,
-        name: 'Chat 1',
-    },
-    {
-        id: 1,
-        name: 'Chat 2',
-    },
-    {
-        id: 2,
-        name: 'Chat 3',
-    },
+  {
+    id: 0,
+    name: 'Chat 1',
+  },
+  {
+    id: 1,
+    name: 'Chat 2',
+  },
+  {
+    id: 2,
+    name: 'Chat 3',
+  },
 ];
 
 const useStyles = makeStyles(() => ({
-    chatList: {
-      width: '100%',
-    },
-    chat: {
-      display: 'flex',
-      flexDirection: 'column',
-    },
-    inputWrapper: {
-      flex: '1',
-      width: '500px',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      backgroundColor: 'lavender',
-      boxSizing: 'border-box',
-      padding: '0 30px',
-    },
-    buttonSend: {
-      borderColor: 'mediumpurple',
-      color: 'mediumpurple',
-      borderRadius: '10px',
-      padding: '7px 15px',
-    },
-    chatItem: {
-        color: 'mediumpurple',
-        padding: '10px 0'
-    }
-  }));
+  chatList: {
+    width: '100%',
+  },
+  chat: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  inputWrapper: {
+    flex: '1',
+    width: '500px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: 'lavender',
+    boxSizing: 'border-box',
+    padding: '0 30px',
+  },
+  buttonSend: {
+    borderColor: 'mediumpurple',
+    color: 'mediumpurple',
+    borderRadius: '10px',
+    padding: '7px 15px',
+  },
+  chatItem: {
+    color: 'mediumpurple',
+    padding: '10px 0'
+  }
+}));
 
 const Chat = () => {
-    const classes = useStyles();
-    const [inputMessage, setInputMessage] = useState('');
-    const [messageList, setMessageList] = useState([]);
+  const classes = useStyles();
+  const [inputMessage, setInputMessage] = useState('');
+  const [messageList, setMessageList] = useState([]);
 
-    const onSendMessage = () => {
-        setMessageList((prev) => [...prev, { text: inputMessage, author: 'user' }]);
-        setInputMessage("");
-    };
+  const onSendMessage = () => {
+    setMessageList((prev) => [...prev, { text: inputMessage, author: 'user' }]);
+    setInputMessage("");
+  };
 
-    useEffect(() => {
-        const timerId = setTimeout(() => {
-          setMessageList((prev) => {
-            if (prev.length && prev[prev.length - 1].author !== 'robot') {
-              return [...prev, { text: prev[prev.length - 1].text, author: 'robot' }];
-            }
-            return prev;
-          });
-    
-          return (() => clearTimeout(timerId));
-        }, 1500);
-    
-      }, [messageList]);
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setMessageList((prev) => {
+        if (prev.length && prev[prev.length - 1].author !== 'robot') {
+          return [...prev, { text: prev[prev.length - 1].text, author: 'robot' }];
+        }
+        return prev;
+      });
 
-    return (
-        <>
-            <div className={classes.chatList}>
-                <MenuList>
-                    {chats.map((chat) => (
-                        <MenuItem key={chat.id} className={classes.chatItem}>{chat.name}</MenuItem>
-                    ))}
-                </MenuList>
-            </div>
+      return (() => clearTimeout(timerId));
+    }, 1500);
 
+  }, [messageList]);
+
+  return (
+    <>
+      <div className={classes.chatList}>
+        <MenuList>
+          {chats.map((chat) => (
+            <MenuItem key={chat.id} className={classes.chatItem}>{chat.name}</MenuItem>
+          ))}
+        </MenuList>
+      </div>
+
+      <Switch>
+          <Route path="/chat/:id">
             <div className={classes.chat}>
-                <MessageList list={messageList} />
+              <MessageList list={messageList} />
 
-                <div className={classes.inputWrapper}>
-                    <InputMessage inputValue={inputMessage} onChangeMessage={setInputMessage} onSendMessage={onSendMessage} />
-                    <button onClick={onSendMessage} className={classes.buttonSend}>Отправить</button>
-                </div>
+              <div className={classes.inputWrapper}>
+                <InputMessage inputValue={inputMessage} onChangeMessage={setInputMessage} onSendMessage={onSendMessage} />
+                <button onClick={onSendMessage} className={classes.buttonSend}>Отправить</button>
+              </div>
             </div>
-        </>
-    );
+          </Route>
+        </Switch>
+    </>
+  );
 };
 
 export default Chat;
