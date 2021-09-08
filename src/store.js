17be9262142bc8da6bbc46f8ac16f666402b6a58
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2021-09-08 18:30:09 +0300

    feat: установить redux-thunk и подключить к стору

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import profileReducer from './components/Profile/profileSlice';
 import chatReducer from './components/Chat/chatSlice';
+import thunkMiddleware from 'redux-thunk';
 
 export default configureStore({
   reducer: {
     profile: profileReducer,
     chat: chatReducer,
   },
+  middleware: [thunkMiddleware]
 });
